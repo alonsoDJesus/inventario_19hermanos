b10b809805615659e8cc42ade78fe7575c192c3f
Alonso De Jesús <alonsodjesus.vargas.rincon@hotmail.com>
2024-04-20 16:43:04 -0600

    menu v1

---
+++ src/frontend/menu-resources.js
@@ -0,0 +1,12 @@
+const icons = {
+    house: '../../icons/house-solid.svg',
+    box: '../../icons/box-solid.svg',
+    truck: '../../icons/truck-solid.svg',
+    user: '../../icons/user-solid.svg',
+    off: '../../icons/power-off-solid.svg',
+}
+
+const links = {
+    home: 'index.html',
+    newSale: 'ventas/nueva-venta.html'
+}
---
+++ src/frontend/menu.js
@@ -0,0 +1,64 @@
+const body = document.body
+
+function goToHome() {
+    location.href = links.home
+    console.log('click')
+}
+
+function goToNewSale() {
+    location.href = links.newSale
+    console.log('click')
+}
+
+body.innerHTML = `
+<nav>
+<div class="nav__container">
+    <ul class="nav__options">
+        <li class="nav__item" onclick="goToHome()">
+            <img src="${icons.house}" alt="" class="nav__icon" id="houseSolid">
+            <p>Inicio</p>
+        </li>
+        <li class="nav__item">
+            <img src="${icons.box}" alt="" class="nav__icon" id="boxSolid">
+            <p>Inventario</p>
+            <ul class="nav__subitems">
+                <li>
+                    <p>Ver todos los productos</p>
+                </li>
+                <li>
+                    <p>Registrar nuevo producto</p>
+                </li>
+            </ul>
+        </li>
+        <li class="nav__item">
+            <img src="${icons.truck}" alt="" class="nav__icon" id="truckSolid">
+            <p>Ventas</p>
+            <ul class="nav__subitems">
+                <li onclick="goToNewSale()">
+                    <p>Nueva venta</p>
+                </li>
+                <li>
+                    <p>Ver ventas iniciadas</p>
+                </li>
+                <li>
+                    <p>Ver ventas finalizadas</p>
+                </li>
+            </ul>
+        </li>
+        <li class="nav__item">
+            <img src="${icons.user}" alt="" class="nav__icon" id="userSolid">
+            <p>Personal</p>
+            <ul class="nav__subitems">
+                <li>
+                    <p>Registrar nuevo personal</p>
+                </li>
+            </ul>
+        </li>
+    </ul>
+
+    <div onclick="" class="button__close">
+        <img src="${icons.off}" alt="" id="powerOffSolid">
+    </div>
+</div>
+</nav>
+`;

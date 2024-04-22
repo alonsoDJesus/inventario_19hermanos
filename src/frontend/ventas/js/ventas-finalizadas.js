1369c1bdcb3cfcc4c91330661082a43392143795
Alonso De Jesús <alonsodjesus.vargas.rincon@hotmail.com>
2024-04-22 16:09:46 -0600

    diseño ya casi terminado y funcional

--- src/frontend/ventas/js/ventas-finalizadas.js
+++ src/frontend/ventas/js/ventas-finalizadas.js
@@ -8,21 +8,30 @@ function renderCompletedSales(completedSales) {
            containerCards.innerHTML += `
            <div class = "card">
 
-               <div class="card_body">
-                   <div class="card_data">
-                       <p class="data data_id">${sale.id}</p>
-                       <p class="data data_nombre">${sale.nombre}</p>
-                       <p class="data data_ruta">${sale.salida}</p>
-                       <p class="data data_time">${sale.llegada}</p>
-                       <p class="data number">${sale.venta}</p>
-                       <p class="data number">${sale.costo}</p>
-                       <p class="data number">${sale.utilidad}</p>
+               <div class="card__body">
+                   <div class="card__data">
+                        <p class="data data_id">${sale.id}</p>
+                        <p class="data data_nombre">${sale.nombre}</p>
+                        <p class="data data_ruta">${sale.ruta}</p>
+                        <div class="data__container itinerary">
+                           <p class= "data__containerheader">Inicio</p>
+                           <p clas= "data__containerheader">Fin</p>
+                           <p class="data_salida">${sale.salida}</p>
+                           <p class="data_llegada">${sale.llegada}</p>
+                        </div>
+                        <div class="data__container summary">
+                           <p class= "data__containerheader">Venta</p>
+                           <p clas= "data__containerheader">Costo</p>
+                           <p class= "data__containerheader">Utilidad</p>
+                           <p class="number">${sale.venta}</p>
+                           <p class="number">${sale.costo}</p>
+                           <p class="number">${sale.utilidad}</p>
+                        </div> 
                    </div>
                </div>
                
            </div>
-       `;
-       });
+      `;});
    }
 }
 

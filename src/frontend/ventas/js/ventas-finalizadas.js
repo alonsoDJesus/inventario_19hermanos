07aa9a7b8f0cf2980ec954e340e151ffdd584f4e
Alonso De Jesús <alonsodjesus.vargas.rincon@hotmail.com>
2024-04-22 14:24:42 -0600

    los datos son mostrados en tarjetas aunque en versión cruda

--- src/frontend/ventas/js/ventas-finalizadas.js
+++ src/frontend/ventas/js/ventas-finalizadas.js
@@ -1,6 +1,34 @@
-async function getCompletedSales(){
+const containerCards = document.getElementById('containerCards')
+
+function renderCompletedSales(completedSales) {
+   if (completedSales.length != 0) {
+       containerCards.innerHTML = ''
+
+       completedSales.forEach(sale => {
+           containerCards.innerHTML += `
+           <div class = "card">
+
+               <div class="card_body">
+                   <div class="card_data">
+                       <p class="data data_id">${sale.id}</p>
+                       <p class="data data_nombre">${sale.nombre}</p>
+                       <p class="data data_ruta">${sale.salida}</p>
+                       <p class="data data_time">${sale.llegada}</p>
+                       <p class="data number">${sale.venta}</p>
+                       <p class="data number">${sale.costo}</p>
+                       <p class="data number">${sale.utilidad}</p>
+                   </div>
+               </div>
+               
+           </div>
+       `;
+       });
+   }
+}
+
+async function getCompletedSales(com){
    const completedSales = await window.electronAPI.selectCompletedSales()
-   console.log(completedSales)
+   renderCompletedSales(completedSales)
 }
 
 getCompletedSales()

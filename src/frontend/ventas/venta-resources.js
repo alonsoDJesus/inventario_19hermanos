c46baedd9be157cd3f964202e3bbb96deef4f201
Alonso De Jesús <alonsodjesus.vargas.rincon@hotmail.com>
2024-05-14 11:23:42 -0600

    nuevo nombre para los recursos de las paginas de ventas

--- src/frontend/ventas/venta-resources.js
+++ src/frontend/ventas/venta-resources.js
@@ -6,6 +6,12 @@ const icons = {
     off: '../../../icons/power-off-solid.svg',
     edit: '../../../icons/pen-to-square-solid.svg',
     delete: '../../../icons/trash-solid.svg',
+    boxesPrimary: '../../../icons/boxes-stacked-solid-primary.svg',
+    bottlePrimary: '../../../icons/bottle-water-solid-primary.svg',
+    truckPrimary: '../../../icons/truck-solid-primary.svg',
+    hourglassStartPrimary: '../../../icons/hourglass-start-solid-primary.svg',
+    hourglassEndPrimary: '../../../icons/hourglass-end-solid-primary.svg',
+    addressCardPrimary: '../../../icons/address-card-solid-primary.svg',
 }
 
 const links = {
